fd16bb6ed7e82888dd3dc45f83ae26e86fec5d23
shinya <in.go.b.shin@gmail.com>
2026-04-20 10:49:34 +0900

    update axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,10 @@ import axios from "axios";
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
-  withXSRFToken: true,
 });
+
+// 👇 これを追加（重要）
+api.defaults.xsrfCookieName = "XSRF-TOKEN";
+api.defaults.xsrfHeaderName = "X-XSRF-TOKEN";
 
 export default api;
